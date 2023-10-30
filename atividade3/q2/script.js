ffd85518b46a2004d932c57df6c98f2380d4bad1
Jose Henrique <jh4832184@gmail.com>
2023-10-30 14:56:05 -0300

    feat: add event submit

--- atividade3/q2/script.js
+++ atividade3/q2/script.js
@@ -13,3 +13,18 @@ class ClienteBanco {
         this.saldo = this.saldo - 100
     }
 }
+let bank = [];
+
+document.querySelector("form").addEventListener("submit", (e)=>{
+    e.preventDefault(); 
+    bank =[...bank, {name:e.target.name.value, document: e.target.document.value, saldo: Number(e.target.balance.value)}];
+})
+
+
+/* const element = document.getElementById("myList").children[0];
+
+// Create a new text node:
+const newNode = document.createTextNode("Water");
+
+// Replace the text node:
+element.replaceChild(newNode, element.childNodes[0]); */
