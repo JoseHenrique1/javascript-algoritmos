ec5f9f9484463269498c25d6dae98eb0648a9adc
Jose Henrique <jh4832184@gmail.com>
2023-11-04 22:30:28 -0300

    feat: add comprar

--- atividade4/lista.js
+++ atividade4/lista.js
@@ -18,12 +18,16 @@ function listar () {
 function remover (produto) {
     let lista = JSON.parse( localStorage.getItem("lista") );
     lista.splice(lista.indexOf(produto), 1);
-    localStorage.setItem("lista", JSON.stringify(lista));
+    localStorage.setItem("lista", JSON.stringify(lista));  
+}
+
+function comprar (id) {
+    let lista = JSON.parse( localStorage.getItem("lista") );
+    let produto = lista.filter((item)=>item.id == id);
+    produto = produto[0]; 
+    lista[lista.indexOf(produto)].comprado = produto.comprado? false: true;
+    localStorage.setItem('lista', JSON.stringify(lista));
 }
 
 
-
-
-
-
-export {adicionar, listar, remover};
+export {adicionar, listar, remover, comprar};
